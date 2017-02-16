03e5337482e000e22a57e8e0ec26d1244b1ef868
Ruxaxup <gustavo.jim.gonz@gmail.com>
2017-02-16 12:07:20 -0600

    First commit
    Creacion de escenas

---
+++ Assets/MainFolder/Scripts/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    public float _ySpeed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, _ySpeed, 0, ForceMode.Impulse);
+        }
+	}
+}
